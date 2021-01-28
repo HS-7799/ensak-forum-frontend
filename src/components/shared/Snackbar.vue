7364f5c76a852613d99b48f965f09fd44645b213
hamza elharsi <hamza.elharsi77@gmail.com>
2021-01-28 11:04:20 +0100

    add snackbar component

---
+++ src/components/shared/Snackbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <v-snackbar
+          v-model="snackbar"
+          :timeout="timeout"
+           color="success"
+          absolute
+          right 
+          top
+          style="top:7%"
+        >
+          {{ $store.getters.getSnackMessage }}
+        </v-snackbar>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data: () => ({
+      snackbar: true,
+      timeout: 2000
+    })
+    
+}
+</script>
